9307b8cf7150d780409ed036dc5193328bd7f2af
G'ayrat <shirnboyevgayrat@gmail.com>
2022-06-18 09:43:37 +0500

    salom

---
+++ arithmetic04.dart
@@ -0,0 +1,11 @@
+void main() {
+  int number;
+  int x1;
+  int x2;
+  int x3;
+  number = 437;
+  x1 = 400;
+  x2 = 30;
+  x3 = 7;
+  print(x1 + x2 + x3);
+}
---
+++ arithmetic06.dart
@@ -0,0 +1,7 @@
+void main() {
+  int number;
+  int answer;
+  number = 43;
+  answer = 21;
+  print(number + answer / 2);
+}
